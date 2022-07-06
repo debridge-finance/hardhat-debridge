464a4c1a80fccb0b2dfdcbf314006220e1b3904e
Alexey Churkin <alx.churkin@gmail.com>
2022-07-06 17:21:19 +0200

    Docs; task reorg

--- README.md
+++ README.md
@@ -1,41 +1,38 @@
 # hardhat-debridge
 
-_A one line description of the plugin_
+**Easily test your integrations with the [deBridge cross-chain protocol](https://debridge.finance).** A plugin for [Hardhat](https://hardhat.org) provides the toolkit for emulating the flow of the bridge, including onchain contracts and off-chain validation activity.
 
-[Hardhat](https://hardhat.org) plugin example.
+## Rationale
 
-## What
+[deBridge](https://debridge.finance) is a generic messaging and cross-chain interoperability protocol that enables decentralized transfers of arbitrary data and assets between various blockchains. Given the complexity of the protocol itself and the number of different components involved in the flow, **integration development** and its sufficient **unit testing** may by very tricky as it typically requires a complete infrastructure setup.
 
-<_A longer, one paragraph, description of the plugin_>
+`hardhat-debridge` provides the toolkit for creating lightweight and blazing-fast emulation environment on top of [hardhat network](https://hardhat.org/hardhat-network), behaving close to how the mainnet setup of the deBridge infrastructure does.
 
-This plugin will help you with world domination by implementing a simple tic-tac-toe in the terminal.
+In a nutshell, this plugin is suitable to validate cross-chain interactions as follows:
+- developing unit test cases for your contracts, where deBridge infrastructure emulator is a part of a runtime (runtime emulation);
+- performing functional/integration tests against deBridge infrastructure emulator running as a local process
 
 ## Installation
 
-<_A step-by-step guide on how to install the plugin_>
+Install the package:
 
 ```bash
-npm install <your npm package name> [list of peer dependencies]
+npm install --save-dev @debridge-finance/hardhat-debridge
 ```
 
 Import the plugin in your `hardhat.config.js`:
 
 ```js
-require("<your plugin npm package name>");
+require("@debridge-finance/hardhat-debridge");
 ```
 
 Or if you are using TypeScript, in your `hardhat.config.ts`:
 
 ```ts
-import "<your plugin npm package name>";
+import "@debridge-finance/hardhat-debridge";
 ```
 
 
-## Required plugins
-
-<_The list of all the required Hardhat plugins if there are any_>
-
-- [@nomiclabs/hardhat-web3](https://github.com/nomiclabs/hardhat/tree/master/packages/hardhat-web3)
 
 ## Tasks
 
@@ -51,29 +48,6 @@ This plugin adds the _example_ task to Hardhat:
 output of `npx hardhat help example`
 ```
 
-## Environment extensions
-
-<_A description of each extension to the Hardhat Runtime Environment_>
-
-This plugin extends the Hardhat Runtime Environment by adding an `example` field
-whose type is `ExampleHardhatRuntimeEnvironmentField`.
-
-## Configuration
-
-<_A description of each extension to the HardhatConfig or to its fields_>
-
-This plugin extends the `HardhatUserConfig`'s `ProjectPathsUserConfig` object with an optional
-`newPath` field.
-
-This is an example of how to set it:
-
-```js
-module.exports = {
-  paths: {
-    newPath: "new-path"
-  }
-};
-```
 
 ## Usage
 
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import "@nomiclabs/hardhat-ethers";
 import "@openzeppelin/hardhat-upgrades";
 import { Event, Signer } from "ethers";
-import { extendEnvironment, task } from "hardhat/config";
+import { extendEnvironment, subtask, task } from "hardhat/config";
 import { lazyObject } from "hardhat/plugins";
+import {
+  TASK_NODE, TASK_NODE_SERVER_READY,
+} from 'hardhat/builtin-tasks/task-names';
 
 import { SentEvent } from "../typechain/IDeBridgeGate";
 
@@ -13,6 +16,12 @@ import {
   GetClaimArgsFunction,
 } from "./functions";
 import "./type-extensions";
+
+
+const TASK_DEBRIDGE_DEPLOY_EMULATOR_CONTRACT = "debridge-deploy-emulator-contract"
+const SUBTASK_DEBRIDGE_DEPLOY_EMULATOR_CONTRACT_AND_RUN_LISTENER = "subtask-debrige-deploy-emulator"
+
+let localArgs: any = null;
 
 export interface DeBridge {
   emulator: {
@@ -53,29 +62,35 @@ extendEnvironment((hre) => {
 });
 
 task(
-  "debridge-deploy-emulator",
+  TASK_DEBRIDGE_DEPLOY_EMULATOR_CONTRACT,
   "Deploys deBridgeGate emulator contract with pre-configured defaults"
 ).setAction(async (args, hre) => {
   const gate = await hre.deBridge.emulator.deployGate();
 
-  console.log(`DeBridgeGate emulator has been deployed at ${gate.address}`);
+  console.log(`DeBridgeGate emulator contract has been deployed at \x1b[31m ${gate.address} \x1b[0m`);
+
+  return gate
 });
 
-task(
-  "debridge-run-emulator",
-  "Deploys deBridgeGate emulator contract and runs a foreground claiming emulation service"
-)
+
+subtask(TASK_NODE_SERVER_READY).setAction(async (args, hre, runSuper) => {
+  await runSuper(args);
+
+  if (localArgs !== null) {
+    await hre.run(localArgs.RUN_TASK, {
+      ...localArgs,
+    });
+  }
+});
+
+subtask(SUBTASK_DEBRIDGE_DEPLOY_EMULATOR_CONTRACT_AND_RUN_LISTENER)
   .addOptionalParam(
     "minExecutionFee",
     "Claiming emulation service will automatically construct and broadcast claiming txns with this minimum execution fee included",
     "0"
   )
   .setAction(async (args, hre) => {
-    const gate = await hre.deBridge.emulator.deployGate();
-
-    console.log(
-      `DeBridgeGate emulator has been deployed at \x1b[31m ${gate.address} \x1b[0m`
-    );
+    const gate = await hre.run(TASK_DEBRIDGE_DEPLOY_EMULATOR_CONTRACT)
 
     // we need to listen for '*' rather than 'event' because the latter doesn't daemonize the process for some reason
     gate.on("*", async (obj: Event) => {
@@ -158,4 +173,27 @@ task(
     return new Promise(() => {
       /*_*/
     });
+  });
+
+
+task(
+  "debridge-run-emulator",
+  "Deploys deBridgeGate emulator contract and runs a foreground claiming emulation service"
+)
+  .addOptionalParam(
+    "minExecutionFee",
+    "Claiming emulation service will automatically construct and broadcast claiming txns with this minimum execution fee included",
+    "0"
+  )
+  .setAction(async (args, hre) => {
+    if (hre.network.name === "hardhat") {
+      localArgs = {
+        ...args,
+        RUN_TASK: SUBTASK_DEBRIDGE_DEPLOY_EMULATOR_CONTRACT_AND_RUN_LISTENER
+      };
+      return hre.run(TASK_NODE);
+    }
+    else {
+      return hre.run(SUBTASK_DEBRIDGE_DEPLOY_EMULATOR_CONTRACT_AND_RUN_LISTENER, args)
+    }
   });
