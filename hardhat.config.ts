ec2ea58c9893c8b8b5d7604de5c26935a30af4f0
Alexey Churkin <alx.churkin@gmail.com>
2022-11-08 10:21:53 +0100

    Contracts now precompiled with optimizer enabled (as on mainnet)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,7 +10,19 @@ import "./src"
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.7",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
   networks: {
     hardhat: {
       allowUnlimitedContractSize: true,
